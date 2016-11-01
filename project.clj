1b212910e61055d7a82ca2aec6d4b14087a916ea
Adam Childs <adchilds@eckerd.edu>
2016-10-31 20:20:28 -0500

    Update project settings.

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject jythonscript "0.1.0-SNAPSHOT"
+(defproject jythonscript "1.0-SNAPSHOT"
 
-  :description "FIXME: write description"
+  :description "A simple Jython wrapper for easier execution and/or evaluation of Python expressions
+  and scripts at runtime for the Clojure programming language."
 
-  :url "http://example.com/FIXME"
+  :url "https://github.com/adchilds/JythonScriptClj"
 
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.python/jython-standalone "2.7.0"]])
